2437f61aedecc5f05de1453358c6bbd7ca756e1c
st67024DvorakAdam <125569481+st67024DvorakAdam@users.noreply.github.com>
2023-05-15 14:14:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+script.js
 // Kalkulátor ceny
 
 function calculatePrice(){
@@ -31,21 +32,16 @@ function calculatePrice(){
   }
 
   var result = roomSize*priceOfFloor;
-  result = result.toFixed(2);
-  
+
   border = document.getElementById("room-border").checked;
   if(border){
     result += (Math.sqrt(roomSize)*4)*borders;
   }
 
-  border = border.toFixed(2);
-  
   care = document.getElementById("floor-care").checked;
   if(care){
     result += roomSize*protection;
   }
-  
-  care = care.toFixed(2);
 
   result = Math.round(result);
   result += " Kč"
